def66f9a9989fe1cf4b6e233300841d6283fe642
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-07-24 15:07:37 +0200

    brush up styles & add meta-tags

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import PageWrapper from '$lib/components/shared/page-wrapper.svelte';
 	import Icons from './Icons.svelte';
+
+	export let data;
 </script>
 
-<PageWrapper
-	title="Icons"
-	description="A collection of icons used across Vercel products. Right click to copy."
->
+<PageWrapper title={data.title} description={data.description}>
 	<!-- Todo: add search field -->
 	<!-- <Search></Search> -->
-
 	<Icons />
 </PageWrapper>
--- src/routes/icons/Icons.svelte
+++ src/routes/icons/Icons.svelte
@@ -1,14 +1,41 @@
 <script lang="ts">
 	import { Icons } from '$lib/assets/icons';
+	import { cn } from '$lib/utils';
+	import { onMount } from 'svelte';
 
-	const preview_icons = Object.entries(Icons).filter(([name]) => name !== 'ErrorStates');
+	const icons_to_exclude = ['BrandAssets', 'ErrorStates'];
+	const preview_icons = Object.entries(Icons).filter(([name]) => !icons_to_exclude.includes(name));
+
+	// e.g. "ArrowUp" -> "arrow-up"
+	function format_name(str: string) {
+		return str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+	}
+
+	let last_row_count = 0;
+
+	const update_last_row_count = () => {
+		last_row_count = window.innerWidth >= 640 ? preview_icons.length % 4 : preview_icons.length % 2;
+	};
+
+	onMount(update_last_row_count);
 </script>
 
-<div class="grid grid-cols-4 divide-x divide-y">
-	{#each preview_icons as [name, IconComponent]}
-		<div class="flex flex-col items-center gap-2 p-2 py-8 hover:bg-gray-100">
-			<svelte:component this={IconComponent} height="15" width="15" class="text-gray-900" />
-			<span class="text-sm text-gray-900">{name.toLowerCase()}</span>
+<svelte:window on:resize={update_last_row_count} />
+
+<section class="grid grid-cols-2 sm:grid-cols-4">
+	{#each preview_icons as [name, icon], i}
+		<div
+			class={cn(
+				'flex h-28 w-full cursor-pointer flex-col items-center justify-center gap-1.5 px-4 text-gray-900 hover:bg-background-100',
+				// Border-specifics.
+				'border-b border-r [&:nth-child(2n)]:border-r-0 sm:[&:nth-child(2n)]:border-r sm:[&:nth-child(4n)]:border-r-0',
+				{
+					'border-b-0': i >= preview_icons.length - last_row_count
+				}
+			)}
+		>
+			<svelte:component this={icon} aria-hidden="true" height="16" width="16" />
+			<span class="text-sm">{format_name(name)}</span>
 		</div>
 	{/each}
-</div>
+</section>
