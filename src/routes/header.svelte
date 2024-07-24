ee5d38571127d7d0ed5ef154c2c76b9fafc3defa
Aidan Bleser <aidanbleser35@gmail.com>
2024-07-24 16:30:49 -0500

    Groundwork for search dialog

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,7 +1,58 @@
 <script lang="ts">
 	import { Icons } from '$lib/assets/icons';
 	import ThemeSwitcher from '$lib/components/shared/theme-switcher.svelte';
+	import { mediaQuery } from 'svelte-legos';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+
+	let open = true;
+	const isDesktop = mediaQuery('(min-width: 768px)');
+
+	const toggleOpen = () => (open = true);
+
+	let search = '';
 </script>
+
+{#if $isDesktop}
+	<Dialog.Root bind:open>
+		<Dialog.Content class="bg-background-100 sm:max-w-[640px] p-0" includeX={false}>
+			<div class="flex h-[53px] place-items-center justify-between border-b px-3">
+				<!-- search -->
+				<input
+					bind:value={search}
+					type="text"
+					class="h-full border-none bg-transparent text-lg outline-none placeholder:text-gray-600 focus:outline-none flex-grow"
+					placeholder="Search..."
+				/>
+				<kbd
+					class="pointer-events-none inline-flex h-5 min-w-5 select-none items-center justify-center gap-1 rounded-md border bg-background-100 px-1 font-mono text-xs font-medium text-gray-900"
+				>
+					Esc
+				</kbd>
+			</div>
+			<div class="h-[436px]"></div>
+		</Dialog.Content>
+	</Dialog.Root>
+{:else}
+	<Drawer.Root bind:open>
+		<Drawer.Content class="h-3/4 bg-background-100 p-0">
+			<div class="flex h-[53px] place-items-center justify-between border-b px-3">
+				<!-- search -->
+				<input
+					bind:value={search}
+					type="text"
+					class="h-full border-none bg-transparent text-lg outline-none placeholder:text-gray-600 focus:outline-none flex-grow"
+					placeholder="Search..."
+				/>
+				<kbd
+					class="pointer-events-none inline-flex h-5 min-w-5 select-none items-center justify-center gap-1 rounded-md border bg-background-100 px-1 font-mono text-xs font-medium text-gray-900"
+				>
+					Esc
+				</kbd>
+			</div>
+		</Drawer.Content>
+	</Drawer.Root>
+{/if}
 
 <header class="sticky top-0 z-20 mx-auto w-full max-w-[1220px]">
 	<div
@@ -20,6 +71,7 @@
 			<button
 				class="hidden h-8 w-[220px] cursor-pointer items-center justify-between rounded-md border border-gray-400 bg-transparent pl-2 pr-1.5 font-sans text-sm text-gray-700 outline-none hover:bg-background-200 focus-visible:shadow-focus-ring xl:flex"
 				type="button"
+				on:click={toggleOpen}
 			>
 				Search Geist
 				<kbd
@@ -32,6 +84,16 @@
 
 			<div class="hidden xl:block">
 				<ThemeSwitcher />
+			</div>
+
+			<div class="flex place-items-center xl:hidden">
+				<button
+					class="flex size-8 place-items-center justify-center rounded-full border"
+					on:click={toggleOpen}
+				>
+					<!-- Make sure to put a search icon here this is just here as a placeholder -->
+					<Icons.ArrowLeft width="16" height="16" aria-hidden="true" />
+				</button>
 			</div>
 		</div>
 	</div>
