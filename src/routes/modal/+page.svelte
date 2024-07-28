a282f08954741fa4cbbf681f28951d147cedbb4e
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-07-28 19:09:40 +0200

    disabled-actions

--- src/routes/modal/+page.svelte
+++ src/routes/modal/+page.svelte
@@ -3,6 +3,8 @@
 	import PageWrapper from '$lib/components/shared/page-wrapper.svelte';
 	import Default from './default.svelte';
 	import default_code from './default.svelte?raw';
+	import DisabledActions from './disabled-actions.svelte';
+	import disabled_actions_code from './disabled-actions.svelte?raw';
 	import SingleButton from './single-button.svelte';
 	import single_button_code from './single-button.svelte?raw';
 	import Sticky from './sticky.svelte';
@@ -22,5 +24,9 @@
 
 	<Demo id="single-button" code={single_button_code}>
 		<SingleButton />
+	</Demo>
+
+	<Demo id="disabled-actions" code={disabled_actions_code}>
+		<DisabledActions />
 	</Demo>
 </PageWrapper>
---
+++ src/routes/modal/disabled-actions.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Modal from '$lib/components/ui/modal';
+</script>
+
+<Modal.Root>
+	<Modal.Trigger let:builder>
+		<Button size="sm" builders={[builder]}>Open Modal</Button>
+	</Modal.Trigger>
+
+	<Modal.Content>
+		<Modal.Header>
+			<Modal.Title>Modal</Modal.Title>
+			<Modal.Description>This is a modal.</Modal.Description>
+		</Modal.Header>
+
+		<p class="px-6 py-3 text-sm">Some content contained within the modal.</p>
+
+		<Modal.Footer>
+			<Button disabled>Submit</Button>
+		</Modal.Footer>
+	</Modal.Content>
+</Modal.Root>
