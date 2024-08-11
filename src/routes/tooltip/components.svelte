b43964be41552e56b14e5eda0d8ae6f820c0622f
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-08-12 01:23:13 +0200

    .

--- src/routes/tooltip/components.svelte
+++ src/routes/tooltip/components.svelte
@@ -17,13 +17,13 @@
 		<Tooltip.Trigger>
 			<Badge variant="gray" size="sm">LEFT</Badge>
 		</Tooltip.Trigger>
-		<Tooltip.Content side="bottom">The Evil Rabbit Jumped over the Fence.</Tooltip.Content>
+		<Tooltip.Content side="left">The Evil Rabbit Jumped over the Fence.</Tooltip.Content>
 	</Tooltip.Root>
 
 	<Tooltip.Root>
 		<Tooltip.Trigger>
 			<Icons.LoaderCircle class="size-4 animate-spin" />
 		</Tooltip.Trigger>
-		<Tooltip.Content side="bottom">The Evil Rabbit Jumped over the Fence.</Tooltip.Content>
+		<Tooltip.Content side="right">The Evil Rabbit Jumped over the Fence.</Tooltip.Content>
 	</Tooltip.Root>
 </div>
