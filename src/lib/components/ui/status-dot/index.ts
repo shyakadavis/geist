e58dd54b731bc24f8f11f6d826df9edba29e0199
SHYAKA Davis <87414827+shyakadavis@users.noreply.github.com>
2024-08-12 16:53:53 +0200

    component: Status Dot (#48)
    Co-authored-by: Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>

---
+++ src/lib/components/ui/status-dot/index.ts
@@ -0,0 +1,23 @@
+import { tv, type VariantProps } from 'tailwind-variants';
+
+export { default as StatusDot } from './status-dot.svelte';
+
+export const status_dot_variants = tv({
+	base: 'size-2.5 rounded-full',
+	variants: {
+		variant: {
+			QUEUED: 'bg-accents-2',
+			BUILDING: 'bg-[#f5a623]',
+			ERROR: 'bg-[#ff0000]',
+			READY: 'bg-teal-900',
+			CANCELED: 'bg-accents-2'
+		}
+	}
+});
+
+type Variant = VariantProps<typeof status_dot_variants>['variant'];
+
+export type Props = {
+	state: Exclude<Variant, undefined>;
+	label?: boolean;
+};
