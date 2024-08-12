d3255a4b8fbc0d5a4900c7b5dbb5a471ed9ec94d
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-08-12 18:58:57 +0200

    component: Show More

---
+++ src/routes/show-more/default.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { ShowMore } from '$lib/components/ui/show-more';
+
+	let expanded = false;
+
+	function toggle() {
+		expanded = !expanded;
+	}
+</script>
+
+<ShowMore {expanded} on:click={toggle} />
