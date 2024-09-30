90368f6e60272476c73a14a20d296f3651965a97
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-09-30 02:18:23 +0200

    fix: argh, why is file name casing always hard to resolve

--- src/routes/icons/+page.svelte
+++ src/routes/icons/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import PageWrapper from '$lib/components/shared/page-wrapper.svelte';
-	import Icons from './icons.svelte';
+	import IconsGallery from './icons-gallery.svelte';
 
 	let { data } = $props();
 </script>
 
 <PageWrapper title={data.title} description={data.description}>
-	<Icons />
+	<IconsGallery />
 </PageWrapper>
