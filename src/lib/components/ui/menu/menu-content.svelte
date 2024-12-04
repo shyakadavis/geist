cbc1e095989e20ed95caf3aab8812823c5594a65
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2024-12-04 17:59:04 +0200

    fix: menu

--- src/lib/components/ui/menu/menu-content.svelte
+++ src/lib/components/ui/menu/menu-content.svelte
@@ -8,11 +8,18 @@
 		children?: Snippet;
 	};
 
-	let { class: class_name = undefined, sideOffset = 4, children, ...rest }: Props = $props();
+	let {
+		class: class_name = undefined,
+		sideOffset = 8,
+		align = 'start',
+		children,
+		...rest
+	}: Props = $props();
 </script>
 
 <MenuPrimitive.Portal>
 	<MenuPrimitive.Content
+		{align}
 		{sideOffset}
 		class={cn(
 			'z-50 min-w-[10rem] rounded-xl bg-background-100 p-2 text-gray-1000 shadow-shadow-menu focus:outline-none',
