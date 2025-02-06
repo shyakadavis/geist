606a0d4bd08e5a7b34975c87761d5854f324a395
0K0 <>
2025-02-06 16:40:02 +0100

    feat: update warning ProjectBanner label to include user reference

--- src/routes/project-banner/warning.svelte
+++ src/routes/project-banner/warning.svelte
@@ -3,4 +3,4 @@
     import ProjectBanner from "$lib/components/ui/project-banner/project-banner.svelte";
 </script>
 
-<ProjectBanner label='This project was rolled back by' icon={Icons.Stop} variant='warning' callToAction={{label: 'Undo Rollback', onClick: () => { alert('Button clicked'); }}} />
+<ProjectBanner label='This project was rolled back by @johnphamous' icon={Icons.Stop} variant='warning' callToAction={{label: 'Undo Rollback', onClick: () => { alert('Button clicked'); }}} />
