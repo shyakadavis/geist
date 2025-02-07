08ab734d36ad9fc18380cd7f86f76dde5037eb3d
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-02-07 14:27:37 +0200

    fix: update property name for text color in Book component to use snake_case

--- src/routes/book/custom-color.svelte
+++ src/routes/book/custom-color.svelte
@@ -4,6 +4,6 @@
 
 <div class="flex gap-8">
 	<Book color="#9D2127" title="How Vercel improves your website's search engine ranking" />
-	<Book color="#7DC1C1" textColor="white" title="Design Engineering at Vercel" variant="simple" />
+	<Book color="#7DC1C1" text_color="white" title="Design Engineering at Vercel" variant="simple" />
 	<Book color="#FED954" title="The user experience of the Frontend Cloud" />
 </div>
