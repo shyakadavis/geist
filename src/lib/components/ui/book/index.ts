57889e8934b3b7055099aaef8bd97d4cc9992c85
Davis SHYAKA <87414827+davis-shyaka@users.noreply.github.com>
2025-02-07 14:31:46 +0200

    adjust opacity for textured variant in Book component

--- src/lib/components/ui/book/index.ts
+++ src/lib/components/ui/book/index.ts
@@ -1,6 +1,6 @@
+import type { Component } from 'svelte';
 import { type VariantProps, tv } from 'tailwind-variants';
 import Root from './book.svelte';
-import type { Component } from 'svelte';
 
 const book_variants = tv({
 	slots: {
@@ -17,7 +17,7 @@ const book_variants = tv({
 		body_illustration:
 			'flex w-full flex-col items-stretch object-cover p-[6.1%] pl-[14.2%] text-[var(--book-text-color)] inline-size',
 		textured:
-			'pointer-events-none absolute inset-0 rounded-[var(--book-border-radius)] bg-cover bg-no-repeat opacity-30 mix-blend-hard-light brightness-110 bg-book-textured',
+			'pointer-events-none absolute inset-0 rounded-[var(--book-border-radius)] bg-cover bg-no-repeat mix-blend-hard-light brightness-100 bg-book-textured',
 		pages:
 			'absolute top-[3px] h-[calc(100%-2*3px)] w-[calc(29cqw-2px)] bg-book-pages transform-book-pages',
 		back: 'absolute left-0 top-0 h-full min-w-[calc(var(--book-width)*1px)] transform-book-back rounded-book'
